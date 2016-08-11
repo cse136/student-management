7bcd256fe4f1c3d3cb12ec0c50c157ab8a489fa1
Jose A. Martinez <jam004@ucsd.edu>
2016-08-10 21:33:25 -0700

    master Added EF mappings and EF. Added Tutor code.

---
+++ Solution136/Repository/TutorRepository.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class TutorRepository
+    {
+        public void InsertTutor(schedule_tutor student, ref List<string> errors)
+        {
+            var context = new cse136Entities();
+            try
+            {
+                context.schedule_tutor.Add(student);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                errors.Add("Error: " + e);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
+        public void UpdateStudent(schedule_tutor student, ref List<string> errors)
+        {
+            var context = new cse136Entities();
+            try
+            {
+                var result_student = context.schedule_tutor.SingleOrDefault(s =>
+                    s.student_id == student.student_id &&
+                    s.course_schedule == student.course_schedule);
+
+                if (result_student != null)
+                {
+                    result_student.availability_day_id = student.availability_day_id;
+                    result_student.availability_time_id = student.availability_time_id;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                errors.Add("Error: " + e);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
+        public void DeleteStudent(string student_id, int schedule_id, ref List<string> errors)
+        {
+            var context = new cse136Entities();
+
+            try
+            {
+                var student = new schedule_tutor()
+                {
+                    student_id = student_id,
+                    schedule_id = schedule_id
+                };
+
+                context.schedule_tutor.Attach(student);
+                context.schedule_tutor.Remove(student);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                errors.Add("Error: " + e);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
+        public ICollection<schedule_tutor> GetStudentDetail(int schedule_id, ref List<string> errors)
+        {
+            var list = new List<schedule_tutor>();
+            var context = new cse136Entities();
+
+            try
+            {
+                list = context.schedule_tutor.Where(st => st.schedule_id == schedule_id).ToList();
+            }
+            catch (Exception e)
+            {
+                errors.Add("Error: " + e);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+
+            return list;
+        }
+    }
+}

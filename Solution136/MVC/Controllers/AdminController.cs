da42ae567b0d5cb75e6fb785e01e0d362937c4b3
Jose A. Martinez <jam004@ucsd.edu>
2016-08-28 13:01:51 -0700

    master Added admin pages. All the functionality we might implement.

--- Solution136/MVC/Controllers/AdminController.cs
+++ Solution136/MVC/Controllers/AdminController.cs
@@ -16,17 +16,27 @@
             return this.View();
         }
 
-        public ActionResult StudentList()
+        public ActionResult AddCoursePrereq()
         {
             return this.View();
         }
 
-        public ActionResult InstructorList()
+        public ActionResult AddTAToScheduledCourse()
         {
             return this.View();
         }
 
-        public ActionResult CreateStudent()
+        public ActionResult CourseDetails()
+        {
+            return this.View();
+        }
+
+        public ActionResult CourseList()
+        {
+            return this.View();
+        }
+
+        public ActionResult CreateCourse()
         {
             return this.View();
         }
@@ -36,27 +46,22 @@
             return this.View();
         }
 
-        public ActionResult CreateTA()
-        {
-            return this.View();
-        }
-
-        public ActionResult EditTA()
-        {
-            return this.View();
-        }
-
         public ActionResult CreateScheduledCourse()
         {
             return this.View();
         }
 
-        public ActionResult AddPrereq()
+        public ActionResult CreateStudent()
         {
             return this.View();
         }
 
-        public ActionResult EditScheduledCourse()
+        public ActionResult CreateTA()
+        {
+            return this.View();
+        }
+
+        public ActionResult EditCourse()
         {
             return this.View();
         }
@@ -66,17 +71,58 @@
             return this.View();
         }
 
+        public ActionResult EditScheduledCourse(string id)
+        {
+            return this.View();
+        }
+
         public ActionResult EditStudent(string id)
         {
             return this.View();
         }
 
-        public ActionResult TAList()
+        public ActionResult EditTA(string id)
         {
             return this.View();
         }
 
-        public ActionResult ViewCoursePrereqs()
+
+        public ActionResult InstructorList()
+        {
+            return this.View();
+        }
+
+        public ActionResult ScheduledCourseList()
+        {
+            return this.View();
+        }
+
+        public ActionResult ScheduledCourseReviews()
+        {
+            return this.View();
+        }
+
+        public ActionResult ScheduledCourseStudentList()
+        {
+            return this.View();
+        }
+
+        public ActionResult StudentDetails()
+        {
+            return this.View();
+        }
+
+        public ActionResult StudentList()
+        {
+            return this.View();
+        }
+
+        public ActionResult TADetails()
+        {
+            return this.View();
+        }
+
+        public ActionResult TAList()
         {
             return this.View();
         }

85dee070bbd95d141661f6d8aa75e64819b067ed
Darren Ando <dando@ucsd.edu>
2016-08-29 21:46:36 -0700

    master knockout for CreateStudent WIP

--- Solution136/MVC/Scripts/136Libraries/ViewModels/StudentViewModel.js
+++ Solution136/MVC/Scripts/136Libraries/ViewModels/StudentViewModel.js
@@ -8,14 +8,14 @@
     this.Initialize = function() {
 
         var viewModel = {
-            id: ko.observable("A0000111"),
-            ssn: ko.observable("555-55-3333"),
-            first: ko.observable("Bruce"),
-            last: ko.observable("Wayne"),
-            email: ko.observable("bwayne@ucsd.edu"),
-            password: ko.observable("password"),
-            shoesize: ko.observable("10"),
-            weight: ko.observable("160"),
+            id: ko.observable(""),
+            ssn: ko.observable(""),
+            first: ko.observable(""),
+            last: ko.observable(""),
+            email: ko.observable(""),
+            password: ko.observable(""),
+            shoesize: ko.observable(""),
+            weight: ko.observable(""),
             add: function (data) {
                 self.CreateStudent(data);
             }

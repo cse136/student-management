94917fc35202331492479dd16f987cd5b9baee1f
Darren Ando <dando@ucsd.edu>
2016-09-01 19:23:43 -0700

    master Tried Adding TAs, still WIP

--- Solution136/MVC/Scripts/136Libraries/ViewModels/TAViewModel.js
+++ Solution136/MVC/Scripts/136Libraries/ViewModels/TAViewModel.js
@@ -1,23 +1,90 @@
 ﻿function TAViewModel() {
 
-    var TAModelObj = new TAModel();
+    var taModelObj = new TAModel();
     var self = this;
     var initialBind = true;
-    var taListViewModel = ko.observableArray();
+    var taViewModel;
 
-    this.Initialize = function () {
+    // Constructor for object
+    var Level = function (displayName, value) {
+        this.displayName = displayName;
+        this.value = value;
+    };
 
-        var viewModel = {
+    this.Initialize = function (id) {
+        self.taViewModel = {
+
             id: ko.observable(""),
             type: ko.observable(""),
             first: ko.observable(""),
             last: ko.observable(""),
+            
+            availableLevels: ko.observableArray([
+                new Level('All', ''),
+                new Level('Upper', 'upper'),
+                new Level('Lower', 'lower'),
+                new Level('Grad', 'grad')
+            ]),
+
+            availableLevelsWithoutAll: ko.observableArray([
+                   new Level('Upper', 'upper'),
+                   new Level('Lower', 'lower'),
+                   new Level('Grad', 'grad')
+            ]),
+
+            selectedLevel: ko.observable(''), // all selected by default
+
+            ta_filter: ko.observable(''),
+
+            taList: ko.observableArray(),
+
+            filter: function () {
+                viewModelObj.Filter(self.taViewModel.ta_filter(),
+                    self.taViewModel.selectedLevel().value,
+                    function (taListData) {
+
+                        self.taViewModel.taList.removeAll();
+
+                        // DTO from the JSON model to the view model. In this case, courseListViewModel doesn't need the "id" attribute
+                        for (var i = 0; i < taListData.length; i++) {
+                            self.taViewModel.taList.push(
+                                {
+                                    id: taListData[i].ta_id,
+                                    type: taListData[i].ta_type_id,
+                                    first: taListData[i].first_name,
+                                    last: taListData[i].last_name
+                                }
+                            );
+                        }
+                    });
+            },
+
             add: function (data) {
-                self.CreateTA(data);
+                var model = {
+                    id: data.viewModel.ta_id(),
+                    type: data.viewModel.ta_type_id(),
+                    first: data.viewModel.first_name(),
+                    last: data.viewModel.last_name()
+                };
+
+                courseModelObj.Add(model, function (result) {
+                    if (result == "ok") {
+                        debugger;
+
+                        alert("Added TA successfully");
+
+                        window.location.href = '/Staff/TAList'; //relative to domain
+
+
+                    } else {
+                        alert("Error occurred");
+                    }
+                });
+
             }
         };
 
-        ko.applyBindings(viewModel, document.getElementById("TAs"));
+        ko.applyBindings(viewModel);
     };
 
     this.CreateTA = function (data) {
